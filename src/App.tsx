763f8e0185ada2e91f307e72d82f7389639b42df
MFathurrohmanMauludin <fathurdiakfari97@gmail.com>
2025-01-28 08:36:03 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
 
- const App = () => {
+const App = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home}></Route>
+        <Route path="/start-chat" Component={Chat}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
