2a1677e3aa260021a7148005f1cc953c7be1ede9
MFathurrohmanMauludin <fathurdiakfari97@gmail.com>
2025-02-06 21:35:17 +0700

    update

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,29 +1,55 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const Chat = () => {
-  const [response, setResponse] = useState<string>("Loading...");
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(false); // State untuk indikator loading
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const genAI = new GoogleGenerativeAI("AIzaSyC-QcqDWwN5dXFJt4W3Qx7ufAJfJKmf_hE");
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  const fetchData = async () => {
+    if (!question.trim()) {
+      setResponse("silakan masukkan pertanyaan anda terlebih dahulu!");
+      return;
+    }
 
-        const result = await model.generateContent("foku");
-        const text = result.response.text(); // Ensure correct data extraction
+    setLoading(true);
+    setResponse(""); // delete answer after user get the result
 
-        setResponse(text);
-      } catch (error) {
-        setResponse("Error fetching response.");
-        console.error("Error:", error);
-      }
-    };
+    try {
+      const genAI = new GoogleGenerativeAI(
+        "AIzaSyC-QcqDWwN5dXFJt4W3Qx7ufAJfJKmf_hE"
+      );
+      const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
-    fetchData();
-  }, []);
+      const result = await model.generateContent("");
+      const text = result.response.text(); // Ensure correct data extraction
 
-  return <span>{response}</span>;
+      setResponse(text);
+    } catch (error) {
+      setResponse("Error fetching response.");
+      console.error("Error:", error);
+    }
+  };
+
+  setLoading(false);
+
+  return (
+    <>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+      />
+
+      <button className="" type="submit" onClick={fetchData}>
+        {loading ? "sedang diproses..." : "kirim"}
+      </button>
+
+      <div className="border border-gray-400 p-2">{response}</div>
+    </>
+  );
 };
 
 export default Chat;
