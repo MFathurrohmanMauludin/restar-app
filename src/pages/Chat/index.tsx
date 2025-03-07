5a0b59b5111e584cbcded23ac264ffc20dca8d5e
MFathurrohmanMauludin <fathurdiakfari97@gmail.com>
2025-03-07 07:51:00 +0700

    update

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -2,7 +2,14 @@ import { useEffect, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NavLink, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCircle,
+  faMagnifyingGlass,
+  faMinus,
+  faPaperPlane,
+  faScroll,
+  faThumbTack,
+} from "@fortawesome/free-solid-svg-icons";
 import { generateUniqueCode } from "../../utils/generatedCode";
 
 interface Ingredient {
@@ -32,8 +39,12 @@ const Chat = () => {
     "Cari resep makanan dan minuman apa hari ini?"
   );
   const [loading, setLoading] = useState(false);
-  const [history, setHistory] = useState<Record<string, { question: string }[]>>({});
-  const {key} = useLocation();
+  const [history, setHistory] = useState<
+    Record<string, { question: string }[]>
+  >(JSON.parse(localStorage.getItem("chatHistory") || "{}"));
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const id = queryParams.get("id"); // Ambil nilai 'id' dari URL
 
   const fetchData = async () => {
     if (!question.trim()) {
@@ -63,7 +74,9 @@ const Chat = () => {
 
       // Save to localStorage
       const uniqueKey = generateUniqueCode();
-      const chatHistory = JSON.parse(localStorage.getItem("chatHistory") || "{}");
+      const chatHistory = JSON.parse(
+        localStorage.getItem("chatHistory") || "{}"
+      );
       chatHistory[uniqueKey] = [{ question, response: parsedResponse }];
       localStorage.setItem("chatHistory", JSON.stringify(chatHistory));
       setHistory(chatHistory); // Update history state
@@ -74,19 +87,51 @@ const Chat = () => {
     setLoading(false);
   };
 
+  // Re-render when history updates
   useEffect(() => {
-    const savedHistory = JSON.parse(localStorage.getItem("chatHistory") || "{}");
-    setHistory(savedHistory);
-  }, []);
-    
+    const chatHistory = JSON.parse(localStorage.getItem("chatHistory") || "{}");
+    setHistory(chatHistory);
+
+    if (id && chatHistory[id]) {
+      setResponse(chatHistory[id][0]?.response || []);
+    } else {
+      setResponse([]);
+    }
+  }, [id]);
+
   return (
     <div className="grid grid-cols-[20%_80%] h-screen max-sm:grid-cols-1 max-md:grid-cols-1">
       {/* History */}
-      <div className="p-4 max-w-[36ch] border-r border-gray-300 max-sm:hidden max-md:hidden">
-        <div className="underline">Riwayat</div>
+      <div className="px-2 py-4 max-w-[36ch] border-r border-gray-300 max-sm:hidden max-md:hidden">
+        <div className="flex items-center justify-between mb-2">
+          <span className="px-2 text-2xl font-semibold">Riwayat</span>
+          <div className="space-x-0">
+            <button
+              className="cursor-pointer w-8 h-8 hover:bg-[#34169E] hover:text-white rounded"
+              onClick={() => console.log("test")}
+              title="Cari riwayat"
+            >
+              <FontAwesomeIcon icon={faMagnifyingGlass} fontSize={16} />
+            </button>
+            <button
+              className="cursor-pointer w-8 h-8 hover:bg-[#34169E] hover:text-white rounded"
+              onClick={() => console.log("test")}
+              title="Tutup navigation"
+            >
+              <FontAwesomeIcon icon={faMinus} fontSize={16} />
+            </button>
+          </div>
+        </div>
         {Object.keys(history).map((key, index) => (
-          <NavLink className="line-clamp-1" to={`/start-chat?id=${key}`} key={index}>
-            {history[key][0]?.question}
+          <NavLink
+            className={`line-clamp-1 px-2 py-1 space-x-1 ${
+              key === id &&
+              "text-white bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] from-purple-300 via-violet-700 to-purple-300"
+            } text-gray-800 hover:text-white hover:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] hover:from-purple-300 hover:via-violet-700 hover:to-purple-300 rounded`}
+            to={`/start-chat?id=${key}`}
+            key={index}
+          >
+            <span className="text-md">{history[key][0]?.question}</span>
           </NavLink>
         ))}
       </div>
@@ -102,25 +147,34 @@ const Chat = () => {
                     {index + 1}. {data.foodName}
                   </span>
                   <p>{data.description}</p>
-                  <span className="text-lg font-semibold mt-2">Resep {data.foodName}</span>
+                  <span className="text-lg font-semibold mt-2">
+                    Resep {data.foodName}
+                  </span>
                   <span>Bahan yang diperlukan:</span>
                   <ul className="list-disc list-inside">
                     {data.ingredient.map((item, idx) => (
-                      <li key={idx}>{item.name} {item.servingSize}</li>
+                      <li key={idx}>
+                        {item.name} {item.servingSize}
+                      </li>
                     ))}
                   </ul>
                   <span className="mt-1 font-semibold">Cara memasak: </span>
                   {data.completeRecept.map((item, idx) => (
-                    <p key={idx}>{item.number}. {item.step}</p>
+                    <p key={idx}>
+                      {item.number}. {item.step}
+                    </p>
                   ))}
                   <span className="font-semibold">
-                    Estimasi biaya yang dikeluarkan: Rp {data.estimatedCostatRupiah}
+                    Estimasi biaya yang dikeluarkan: Rp{" "}
+                    {data.estimatedCostatRupiah}
                   </span>
                 </div>
               ))}
             </div>
           ) : (
-            <span>Ciptakan resep baru atau jelajahi yang sudah ada dengan kami</span>
+            <span>
+              Ciptakan resep baru atau jelajahi yang sudah ada dengan kami
+            </span>
           )}
         </div>
 
@@ -141,14 +195,23 @@ const Chat = () => {
               title="kirim"
             >
               {loading ? (
-                <FontAwesomeIcon fontSize={16} icon={faCircle} />
+                <FontAwesomeIcon
+                  className="animate-ping"
+                  fontSize={16}
+                  icon={faCircle}
+                />
               ) : (
-                <FontAwesomeIcon className="paper" icon={faPaperPlane} fontSize={16} />
+                <FontAwesomeIcon
+                  className="paper"
+                  icon={faPaperPlane}
+                  fontSize={16}
+                />
               )}
             </button>
           </div>
           <div className="text-center py-1 text-sm">
-            <span className="font-bold">RAI</span> bisa membuat kesalahan, cek info lebih lanjut.
+            <span className="font-bold">RAI</span> bisa membuat kesalahan, cek
+            info lebih lanjut.
           </div>
         </div>
       </div>
